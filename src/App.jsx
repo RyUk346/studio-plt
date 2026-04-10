f8c7a4c826682544d9248cf06f5b0735b971f46f
RyUk346 <mahmudur346@gmail.com>
2026-04-11 01:52:15 +0600

    video deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import BackgroundVideo from "./component/BackgroundVideo";
+// import BackgroundVideo from "./component/BackgroundVideo";
 import ScheduleBoard from "./component/ScheduleBoard";
 
 export default function App() {
   return (
     <div className="relative min-h-screen w-screen overflow-hidden">
-      <BackgroundVideo />
+      {/* <BackgroundVideo /> */}
 
       <div className="relative z-10 min-h-screen">
         <ScheduleBoard />
