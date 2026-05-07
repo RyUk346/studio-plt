6fba8ce1b7cca9060063e471eae5d33029b79978
RyUk346 <mahmudur346@gmail.com>
2026-05-08 00:58:58 +0600

    port update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ScheduleBoard from "./component/ScheduleBoard";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/StudioPLT">
       <Routes>
         <Route path="/StudioPLT/PLT-OP-LP/Layer1" element={<ScheduleBoard />} />
         <Route
